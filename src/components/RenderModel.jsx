c4c1f4decb3402b7a09f7b3cfa3a86fb0fc9f2d2
Vindhya R <166674902+vindhya-14@users.noreply.github.com>
2025-11-08 15:57:37 +0530

    changed

--- src/components/RenderModel.jsx
+++ src/components/RenderModel.jsx
@@ -3,14 +3,29 @@ import { Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import clsx from "clsx";
 import React, { Suspense } from "react";
+import * as WEBGL from "three/examples/jsm/capabilities/WebGL.js";
 
 const RenderModel = ({ children, className }) => {
+  const isWebGLAvailable = WEBGL.isWebGLAvailable();
+
+  if (!isWebGLAvailable) {
+    const message = WEBGL.getWebGLErrorMessage();
+    return (
+      <div
+        className={clsx(
+          "flex flex-col items-center justify-center w-screen h-screen text-white text-center bg-black/70",
+          className
+        )}
+        dangerouslySetInnerHTML={{ __html: message.outerHTML }}
+      />
+    );
+  }
+
   return (
     <Canvas
       className={clsx("w-screen h-screen -z-10 relative", className)}
       shadows={false}
       dpr={[1, 2]}
-      // dpr is the device pixel ratio. Here we are setting it to 1 and 2 for retina displays to prevent blurriness in the model rendering on high resolution screens.
     >
       <Suspense fallback={null}>{children}</Suspense>
       <Environment preset="dawn" />
